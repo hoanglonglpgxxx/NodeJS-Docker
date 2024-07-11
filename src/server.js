d55e1bcb562129c0ccd257b3587c75ee714f77da
Mits <hoanglonglpgxxx@gmail.com>
2024-07-11 17:32:33 +0700

    up

--- src/server.js
+++ src/server.js
@@ -5,15 +5,15 @@ const dotenv = require('dotenv').config();
 const webRoutes = require('./routes/web');
 
 const port = process.env.PORT || 3000;
-const hostname = process.env.HOST || 'localhost';
 
 //Serving static files
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Define routes
 app.use('/', webRoutes);
 
-app.listen(port, hostname, () => {
+app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
