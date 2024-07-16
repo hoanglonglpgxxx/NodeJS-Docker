52af1dab0cd5a6a40c2d12271efa854443a6cb85
Mits <hoanglonglpgxxx@gmail.com>
2024-07-16 09:46:13 +0700

    connect mysql

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const path = require('node:path');
 const app = express();
 const dotenv = require('dotenv').config();
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
 const port = process.env.PORT || 3000;
 
@@ -13,6 +14,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Define routes
 app.use('/', webRoutes);
+
+//test mysql connection
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, results, fields) {
+        console.log('>>> results', results); // results contains rows returned by server
+        console.log('>>> fields', fields); // fields contains extra meta data about results, if available
+    }
+);
+
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
