4297f3414b99c31332a1a08eeeb1e0bde58e7d49
Mits <hoanglonglpgxxx@gmail.com>
2024-07-16 09:57:25 +0700

    move connection to new file

--- src/server.js
+++ src/server.js
@@ -3,8 +3,7 @@ const path = require('node:path');
 const app = express();
 const dotenv = require('dotenv').config();
 const webRoutes = require('./routes/web');
-const mysql = require('mysql2');
-
+const connection = require('./config/db');
 const port = process.env.PORT || 3000;
 
 //Serving static files
@@ -16,14 +15,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', webRoutes);
 
 //test mysql connection
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
 connection.query(
     'SELECT * FROM Users u',
     function (err, results, fields) {
